b2ec31e927c5dc8140054ba05fcd57b07753dddc
Lucas Damaceno <lucas.damaceno@cwi.com.br>
2017-07-07 15:40:23 -0300

    feat(Celler_Feed_FrontEnd):adicionar layout completo.

--- FrontEnd/feed.controller.js
+++ FrontEnd/feed.controller.js
@@ -1,5 +1,22 @@
 modulo.controller('FeedController', function ($scope, authService, $routeParams, $location) {
   
-
+$scope.habilitarNotificacoes = false;
+    $scope.buscar = buscar;
+    
+    function buscar(busca){
+        var arrayTipos = [];
+        if(busca.eventos == true){
+            arrayTipos.push('Eventos');
+        }
+        if(busca.anuncios == true){
+            arrayTipos.push('Anuncios');
+        }
+         if(busca.vaquinhas == true){
+            arrayTipos.push('Vaquinhas');
+        }
+        
+        var objetoBusca = {nome: busca.nome, tipos:arrayTipos};
+        console.log(objetoBusca);
+    }
 
 });
