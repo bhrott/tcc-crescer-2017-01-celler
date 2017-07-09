5fa28e13c9c27e9519042d9bdf220129a141f3f0
Mathias <mathias.ody@cwi.com.br>
2017-07-09 03:26:18 -0300

    feat(feed): filtro de feed funcional.

--- Celler/Celler.Infraestrutura/Repositorios/AnuncioRepositorio.cs
+++ Celler/Celler.Infraestrutura/Repositorios/AnuncioRepositorio.cs
@@ -51,6 +51,45 @@ namespace Celler.Infraestrutura.Repositorios
             return anuncios;
         }
 
+        public object ObterUltimosAnuncios(string[] filtros)
+        {
+            //
+            // Devido ao fato de arrays e listas não poderem ser usadas dentro de expressões LINQ, variáveis temporárias foram criadas.
+            //
+            string tmpFiltro0 = filtros[0];
+            string tmpFiltro1 = filtros[1];
+            string tmpFiltro2 = filtros[2];
+
+            List<AnuncioModel> anuncios = contexto.Anuncio
+                .Include(a => a.Criador)
+                .Include(a => a.Comentarios)
+                .OrderByDescending(a => a.DataAnuncio)
+                .Take(9)
+                .ToList()
+                .AsEnumerable()
+                .Select(a => new AnuncioModel(a.Id,
+                                               a.Titulo,
+                                               a.Descricao,
+                                               a.DataAnuncio,
+                                               a.TipoAnuncio,
+                                               a.Foto1,
+                                               a.Foto2,
+                                               a.Foto3,
+                                               a.Criador.Nome,
+                                               a.Comentarios.Count,
+                                               0))
+
+                .Where(a=> 
+                      (a.TipoAnuncio.ToUpper() == tmpFiltro0.ToUpper()) ||
+                      (tmpFiltro1 != null ? a.TipoAnuncio.ToUpper() == tmpFiltro1.ToUpper() : false) ||
+                      (tmpFiltro2 != null ? a.TipoAnuncio.ToUpper() == tmpFiltro2.ToUpper() : false))
+                .ToList();
+
+            PreencherNumeroDeInteressados(anuncios);
+
+            return anuncios;
+        }
+
         private void PreencherNumeroDeInteressados(List<AnuncioModel> anuncios)
         {
             foreach (AnuncioModel anuncio in anuncios)
