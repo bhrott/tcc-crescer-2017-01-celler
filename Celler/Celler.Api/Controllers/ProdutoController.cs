78b6086283954d762e694ef1cb5d88a195ae7c3e
lucas.gaspar <lucas.gaspar@cwi.com.br>
2017-07-10 19:47:58 -0300

    feat(InteresseProduto): Implementado controller. Repositório ainda imcompleto

---
+++ Celler/Celler.Api/Controllers/ProdutoController.cs
@@ -0,0 +1,34 @@
+﻿using Celler.Api.App_Start;
+using Celler.Infraestrutura;
+using Celler.Infraestrutura.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Celler.Api.Controllers
+{
+    [BasicAuthorization]
+    [RoutePrefix("api/produto")]
+
+    public class ProdutoController : ApiController
+    {
+        readonly ProdutoRepositorio _produtoRepositorio;
+        readonly Contexto _contexto = new Contexto();
+
+        public ProdutoController()
+        {
+            _produtoRepositorio = new ProdutoRepositorio(_contexto);
+
+        }
+
+        [HttpPost, Route("interessado")]
+        public IHttpActionResult SalvarInteressadoProduto(int idUsuario, int idProduto)
+        {
+            _produtoRepositorio.SalvarInteressadoProduto(idUsuario, idProduto);
+            return Ok();
+        }
+    }
+}
---
+++ Celler/Celler.Infraestrutura/Repositorios/ProdutoRepositorio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celler.Infraestrutura.Repositorios
+{
+    public class ProdutoRepositorio
+    {
+        readonly Contexto _contexto;
+
+        public ProdutoRepositorio(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public void Dispose()
+        {
+            _contexto.Dispose();
+        }
+
+        public void SalvarInteressadoProduto(int idUsuario, int idProduto)
+        {
+            //Não implementado
+        }
+    }
+}
