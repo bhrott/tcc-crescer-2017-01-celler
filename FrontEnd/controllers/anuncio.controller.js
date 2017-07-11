34e0540bba54d8f336d232f23978c403129f1e84
Lucas Damaceno <lucas.damaceno@cwi.com.br>
2017-07-11 16:11:33 -0300

    feat(Celler_FrontEnd):permitir visualização de interessados no anuncio

--- FrontEnd/controllers/anuncio.controller.js
+++ FrontEnd/controllers/anuncio.controller.js
@@ -1,9 +1,10 @@
 modulo.controller('AnuncioController', function ($scope, authService, postService, detalheService, $routeParams, $location, $localStorage) {
-    
-     if(!authService.isAutenticado()){
+
+    if(!authService.isAutenticado()){
         $location.path("#!/login");
     }
-    
+    $scope.exibirAModal = exibirModal;
+    $scope.esconderAModal = esconderModal;
     var idAnuncioEspecifico = $routeParams.idAnuncio;
     $scope.idAnuncio = idAnuncioEspecifico;
     $scope.postar = postar;
@@ -26,17 +27,30 @@ modulo.controller('AnuncioController', function ($scope, authService, postServic
             }
             $scope.anuncioEspecifico = response.data.dados;
             console.log($scope.anuncioEspecifico);
+            checarInteresse();
         }
 
     );
 
+    function checarInteresse(){
+
+        $scope.temInteresse = $scope.anuncioEspecifico.Interessados.some( x=> x.Id == $localStorage.usuarioLogado.Id );
+
+    }
+    function exibirModal(){
+        $scope.exibirModal = true;
+    }
+    
+     function esconderModal(){
+        $scope.exibirModal = false;
+    }
 
     function postar(texto){
         postService.postarComentario(texto, idAnuncioEspecifico).then(
 
             function(response){
                 console.log(response.data.dados);
-                
+
                 $scope.produto.Comentario = '';
 
             },
