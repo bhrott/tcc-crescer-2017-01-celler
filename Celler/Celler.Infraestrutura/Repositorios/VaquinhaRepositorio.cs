6b4e242f3a4372ed1a2fedb7492be4f84b378512
lucas.gaspar <lucas.gaspar@cwi.com.br>
2017-07-11 20:22:00 -0300

    feat(Doacao): Doação implementada

---
+++ Celler/Celler.Infraestrutura/Repositorios/VaquinhaRepositorio.cs
@@ -0,0 +1,38 @@
+﻿using Celler.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celler.Infraestrutura.Repositorios
+{
+    public class VaquinhaRepositorio
+    {
+        readonly Contexto _contexto;
+
+        public VaquinhaRepositorio(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public void Dispose()
+        {
+            _contexto.Dispose();
+        }
+
+        public void Alterar(Vaquinha vaquinha)
+        {
+            _contexto.Entry(vaquinha).State = EntityState.Modified;
+        }
+
+        public Vaquinha ObterPorId(int id)
+        {
+            return _contexto.Vaquinha
+                .Include(v => v.Doadores)
+                .Include(v => v.Criador)
+                .FirstOrDefault(v => v.Id == id);
+        }
+    }
+}
