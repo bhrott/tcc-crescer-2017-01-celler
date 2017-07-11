058cc5c560d72f712568d81542ec419a496b8fb1
Mathias <mathias.ody@cwi.com.br>
2017-07-10 22:39:56 -0300

    feat(usuario): adicionar controller para registrar usuario para acelerar e facilitar a população de usuários com hash no backend.

--- Celler/Celler.Api/Controllers/UsuarioController.cs
+++ Celler/Celler.Api/Controllers/UsuarioController.cs
@@ -1,5 +1,7 @@
 ﻿using Celler.Api.App_Start;
+using Celler.Dominio.Entidades;
 using Celler.Infraestrutura;
+using Celler.Infraestrutura.Entidades;
 using Celler.Infraestrutura.Repositorios;
 using System;
 using System.Collections.Generic;
@@ -15,7 +17,7 @@ namespace Celler.Api.Controllers
     [EnableCors(origins: "*", headers: "*", methods: "*")]
     [RoutePrefix("api/usuario")]
 
-    public class UsuarioController : ApiController
+    public class UsuarioController : ControllerBasica
     {
         readonly UsuarioRepositorio _usuarioRepositorio;
         readonly Contexto _contexto = new Contexto();
@@ -34,6 +36,30 @@ namespace Celler.Api.Controllers
             if (usuario == null)
                 return Request.CreateResponse(HttpStatusCode.BadRequest, new { mensagem = "Usuario/Email não cadastrados." });
             return Request.CreateResponse(HttpStatusCode.OK, new { dados = new { Nome = usuario.Nome, Email = usuario.Email } });
+        }
+
+        [HttpPost, Route("registrar")]
+        public HttpResponseMessage Registrar([FromBody]UsuarioModel model)
+        {
+            if (_usuarioRepositorio.Obter(model.Email) == null)
+            {
+                var usuario = new Usuario(model.Nome, model.Email, model.Senha);
+
+                if (usuario.Validar())
+                {
+                    _usuarioRepositorio.Criar(usuario);
+                }
+                else
+                {
+                    return ResponderErro(usuario.Mensagens);
+                }
+            }
+            else
+            {
+                return ResponderErro("Usuário já existe.");
+            }
+
+            return ResponderOk();
         }
 
         protected override void Dispose(bool disposing)
