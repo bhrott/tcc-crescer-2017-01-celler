c8cda690c30806b8881ede7312e3fa594e0ca88c
Mathias <mathias.ody@cwi.com.br>
2017-07-12 22:44:48 -0300

    feat(notificacoes): adicionar salvar e obter notificacoes.

--- Celler/Celler.Api/Controllers/NotificacaoController.cs
+++ Celler/Celler.Api/Controllers/NotificacaoController.cs
@@ -1,6 +1,7 @@
 ﻿using Celler.Infraestrutura;
 using Celler.Infraestrutura.Repositorios;
 using System.Net.Http;
+using System.Threading;
 using System.Web.Http;
 
 namespace Celler.Api.Controllers
@@ -8,6 +9,7 @@ namespace Celler.Api.Controllers
     public class NotificacaoController : ControllerBasica
     {
         readonly NotificacaoRepositorio _notificacaoRepositorio;
+        readonly UsuarioRepositorio _usuarioRepositorio;
         readonly Contexto _contexto = new Contexto();
 
         public NotificacaoController()
@@ -18,7 +20,17 @@ namespace Celler.Api.Controllers
         [HttpGet, Route("notificacoes")]
         public HttpResponseMessage ObterNotificacoes()
         {
-            return null;
+            var usuarioLogado = _usuarioRepositorio.Obter(Thread.CurrentPrincipal.Identity.Name);
+            var notificacoes = _notificacaoRepositorio.ObterNotificacoes(usuarioLogado);
+            return ResponderOk(notificacoes);
+        }
+
+        [HttpGet, Route("notificacoes")]
+        public HttpResponseMessage ObterNotificacoes()
+        {
+            var usuarioLogado = _usuarioRepositorio.Obter(Thread.CurrentPrincipal.Identity.Name);
+            var notificacoes = _notificacaoRepositorio.ObterNotificacoes(usuarioLogado);
+            return ResponderOk(notificacoes);
         }
 
         protected override void Dispose(bool disposing)
@@ -26,6 +38,7 @@ namespace Celler.Api.Controllers
             if (disposing)
             {
                 _notificacaoRepositorio.Dispose();
+                _usuarioRepositorio.Dispose();
             }
             base.Dispose(disposing);
         }
--- Celler/Celler.Infraestrutura/Repositorios/NotificacaoRepositorio.cs
+++ Celler/Celler.Infraestrutura/Repositorios/NotificacaoRepositorio.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Celler.Dominio.Entidades;
+using System.Linq;
+using System.Data.Entity;
 
 namespace Celler.Infraestrutura.Repositorios
 {
@@ -9,6 +11,21 @@ namespace Celler.Infraestrutura.Repositorios
         public NotificacaoRepositorio(Contexto contexto)
         {
             _contexto = contexto;
+        }
+
+        public dynamic ObterNotificacoes(Usuario usuario)
+        {
+            return _contexto.Notificacao
+                            .Include(x => x.Usuario)
+                            .Select(x => new { texto = x.Texto,
+                                               link = x.Link,
+                                               status = x.Status});
+        }
+
+        public void CriarNotificacao(Notificacao notificacao)
+        {
+            _contexto.Notificacao.Add(notificacao);
+            _contexto.SaveChanges();
         }
 
         public void Dispose()
