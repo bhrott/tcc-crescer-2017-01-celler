f64f2127404b1d5af95e75727a294179db03c61c
mathiasody <mathias.ody@cwi.com.br>
2017-07-13 10:28:47 -0300

    feat(notificacao): Notificações via slack e e-mail funcionando

---
+++ Celler/Celler.Infraestrutura/Servicos/EnviarEmail.cs
@@ -0,0 +1,34 @@
+﻿using Celler.Dominio.Models;
+using System;
+using System.Net.Mail;
+
+namespace Celler.Infraestrutura.Servicos
+{
+    public class EnviarEmail
+    {
+        public void enviar(String email, MensagemModel model)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("cellercwi@gmail.com");
+                mail.To.Add(email);
+                mail.Subject = model.Assunto;
+                mail.Body = model.Texto;
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("cellercwi@gmail.com", "cellercwi.10");
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+    }
+}
---
+++ Celler/Celler.Infraestrutura/Servicos/Notificar.cs
@@ -0,0 +1,112 @@
+﻿using Celler.Dominio.Entidades;
+using Celler.Dominio.Models;
+
+namespace Celler.Infraestrutura.Servicos
+{
+    public class Notificar
+    {
+        public Usuario Usuario { get; set; }
+        public Anuncio Anuncio { get; set; }
+        public Usuario UsuarioNotificar { get; set; }
+
+        public Notificar(Usuario usuario, Anuncio anuncio, Usuario usuarioNotificar)
+        {
+            this.Usuario = usuario;
+            this.Anuncio = anuncio;
+            this.UsuarioNotificar = usuarioNotificar;
+        }
+
+        public void NotificarUsuarioComentario()
+        {
+            if (this.UsuarioNotificar.NotificacaoComentarioAnuncioEmail == true)
+            {
+                EnviarEmail email = new EnviarEmail();
+                MensagemModel modelEmail = new MensagemModel("Celler", this.Usuario.Nome + " comentou no anúncio: " + this.Anuncio.Titulo);
+                email.enviar(this.UsuarioNotificar.Email, modelEmail);
+            }
+
+            if (this.UsuarioNotificar.NotificacaoComentarioAnuncioSlack == true)
+            {
+                EnviarMensagemSlack enviar = new EnviarMensagemSlack(this.UsuarioNotificar.CanalSlack, this.Usuario.Nome + " comentou no seu anúncio: " + this.Anuncio.Titulo);
+            }
+        }
+
+        public void NotificarUsuarioEvento()
+        {
+            if (this.UsuarioNotificar.NotificacaoPresencaEmail == true)
+            {
+                EnviarEmail email = new EnviarEmail();
+                MensagemModel modelEmail = new MensagemModel("Celler", this.Usuario.Nome + " confirmou presença no evento: " + this.Anuncio.Titulo);
+                email.enviar(this.UsuarioNotificar.Email, modelEmail);
+            }
+
+            if (this.UsuarioNotificar.NotificacaoPresencaSlack == true)
+            {
+                EnviarMensagemSlack enviar = new EnviarMensagemSlack(this.UsuarioNotificar.CanalSlack, this.Usuario.Nome + " confirmou presença no evento: " + this.Anuncio.Titulo);
+            }
+        }
+
+        public void NotificarUsuarioDesistirEvento()
+        {
+            if (this.UsuarioNotificar.NotificacaoPresencaEmail == true)
+            {
+                EnviarEmail email = new EnviarEmail();
+                MensagemModel modelEmail = new MensagemModel("Celler", this.Usuario.Nome + " desistiu do evento: " + this.Anuncio.Titulo);
+                email.enviar(this.UsuarioNotificar.Email, modelEmail);
+            }
+
+            if (this.UsuarioNotificar.NotificacaoPresencaSlack == true)
+            {
+                EnviarMensagemSlack enviar = new EnviarMensagemSlack(this.UsuarioNotificar.CanalSlack, this.Usuario.Nome + " desistiu do evento: " + this.Anuncio.Titulo);
+            }
+        }
+
+
+
+        public void NotificarUsuarioInteresse()
+        {
+            if (this.UsuarioNotificar.NotificacaoInteresseEmail == true)
+            {
+                EnviarEmail email = new EnviarEmail();
+                MensagemModel modelEmail = new MensagemModel("Celler", this.Usuario.Nome + " se interessou no seu evento: " + this.Anuncio.Titulo);
+                email.enviar(this.UsuarioNotificar.Email, modelEmail);
+            }
+
+            if (this.UsuarioNotificar.NotificacaoInteresseSlack == true)
+            {
+                EnviarMensagemSlack enviar = new EnviarMensagemSlack(this.UsuarioNotificar.CanalSlack, this.Usuario.Nome + " se interessou no seu evento: " + this.Anuncio.Titulo);
+            }
+        }
+
+        public void NotificarUsuarioDesinteresse()
+        {
+            if (this.UsuarioNotificar.NotificacaoInteresseEmail == true)
+            {
+                EnviarEmail email = new EnviarEmail();
+                MensagemModel modelEmail = new MensagemModel("Celler", this.Usuario.Nome + " cancelou o interesse no seu: " + this.Anuncio.Titulo);
+                email.enviar(this.UsuarioNotificar.Email, modelEmail);
+            }
+
+            if (this.UsuarioNotificar.NotificacaoInteresseSlack == true)
+            {
+                EnviarMensagemSlack enviar = new EnviarMensagemSlack(this.UsuarioNotificar.CanalSlack, this.Usuario.Nome + " cancelou o interesse no seu: " + this.Anuncio.Titulo);
+            }
+        }
+
+        public void NotificarUsuarioDoacaoVaquinha()
+        {
+            if (this.UsuarioNotificar.NotificacaoDoacaoVaquinhaEmail == true)
+            {
+                EnviarEmail email = new EnviarEmail();
+                MensagemModel modelEmail = new MensagemModel("Celler", this.Usuario.Nome + " doou para a vaquinha: " + this.Anuncio.Titulo);
+                email.enviar(this.UsuarioNotificar.Email, modelEmail);
+            }
+
+            if (this.UsuarioNotificar.NotificacaoDoacaoVaquinhaSlack == true)
+            {
+                EnviarMensagemSlack enviar = new EnviarMensagemSlack(this.UsuarioNotificar.CanalSlack, this.Usuario.Nome + " Doou para a vaquinha: " + this.Anuncio.Titulo);
+            }
+
+        }
+    }
+}
